d7b6d46decdbb74078b0c9d0318046963891b557
Serge Desmedt <sergedesmedt@yahoo.com>
2018-03-21 22:15:46 +0100

    Runner physics

--- ARRunner.Xamarin/Game/GestureManager.cs
+++ ARRunner.Xamarin/Game/GestureManager.cs
@@ -19,7 +19,6 @@ namespace ARRunner.Xamarin.Game
 
         UIView view;
         TouchType currentTouchType = TouchType.None;
-        //List<(IntPtr Id, CGPoint Point)> currentTouches = new List<(IntPtr, CGPoint)>();
         Dictionary<IntPtr, CGPoint> touchDictionary = new Dictionary<IntPtr, CGPoint>();
         List<IntPtr> touchList = new List<IntPtr>();
 
@@ -33,7 +32,7 @@ namespace ARRunner.Xamarin.Game
 
         public void TouchesBegan(NSSet touches, UIEvent evt)
         {
-            Debug.WriteLine("TouchesBegan: " + touches.Count);
+            //Debug.WriteLine("TouchesBegan: " + touches.Count);
 
             if(touches.Count == 1 && currentTouchType == TouchType.None)
             {
@@ -41,16 +40,13 @@ namespace ARRunner.Xamarin.Game
                 var touch = (UITouch)touches.ElementAt(0);
                 var handle = touch.Handle;
                 var viewTouchPoint = touch.LocationInView(view);
-                //currentTouches.Add((touch.Handle, viewTouchPoint));
-                Debug.WriteLine("TouchesBegan (TouchType.None): " + touches.Count + ", TouchType: " + currentTouchType + ", Handle: " + handle + ", Point: " + viewTouchPoint);
+                //Debug.WriteLine("TouchesBegan (TouchType.None): " + touches.Count + ", TouchType: " + currentTouchType + ", Handle: " + handle + ", Point: " + viewTouchPoint);
 
                 touchDictionary.Add(handle, viewTouchPoint);
                 touchList.Add(handle);
                 if(SingleFingerTouchEvent != null)
                 {
-                    //var firstViewTouchPoint = currentTouches[0].Point;
                     var firstViewTouchPoint = touchDictionary[touchList[0]];
-                    //var touch = (UITouch)touches.First();
                     var touchData = new SingleFingerTouch(0, firstViewTouchPoint, GestureState.Start);
                     SingleFingerTouchEvent(this, new EventArgs<SingleFingerTouch>(touchData));
                 }
@@ -58,25 +54,20 @@ namespace ARRunner.Xamarin.Game
             }
             else if(touches.Count == 1 && currentTouchType == TouchType.SingleTouch)
             {
-                //var existingTouch = currentTouches[0];
                 var touch = (UITouch)touches.ElementAt(0);
                 var handle = touch.Handle;
                 var viewTouchPoint = touch.LocationInView(view);
-                //currentTouches.Add((touch.Handle, viewTouchPoint));
                 //Debug.WriteLine("TouchesBegan: " + touches.Count + ", TouchType.SingleTouch, Handle: " + handle + ", Point: " + viewTouchPoint);
                 touchDictionary.Add(handle, viewTouchPoint);
                 touchList.Add(handle);
                 if (SingleFingerTouchEvent != null)
                 {
-                    //var existingViewTouchPoint = currentTouches[0].Point;
                     var existingViewTouchPoint = touchDictionary[touchList[0]];
                     var touchData = new SingleFingerTouch(0, existingViewTouchPoint, GestureState.Cancelled);
                     SingleFingerTouchEvent(this, new EventArgs<SingleFingerTouch>(touchData));
                 }
                 if(TwoFingerTouchEvent != null)
                 {
-                    //var firstViewTouchPoint = currentTouches[0].Point;
-                    //var secondViewTouchPoint = currentTouches[1].Point;
                     var firstViewTouchPoint = touchDictionary[touchList[0]];
                     var secondViewTouchPoint = touchDictionary[touchList[1]];
 
@@ -97,11 +88,8 @@ namespace ARRunner.Xamarin.Game
                     touchList.Add(handle);
                 }
 
-                //currentTouches.AddRange(touches.Cast<UITouch>().Select(a => (a.Handle, a.LocationInView(view))));
                 if (TwoFingerTouchEvent != null)
                 {
-                    //var firstViewTouchPoint = currentTouches[0].Point;
-                    //var secondViewTouchPoint = currentTouches[1].Point;
                     var firstViewTouchPoint = touchDictionary[touchList[0]];
                     var secondViewTouchPoint = touchDictionary[touchList[1]];
 
@@ -112,7 +100,6 @@ namespace ARRunner.Xamarin.Game
             }
             else if(currentTouchType == TouchType.DoubleTouch)
             {
-                //currentTouches.AddRange(touches.Cast<UITouch>().Select(a => (a.Handle, a.LocationInView(view))));
                 foreach (UITouch touch in touches.Cast<UITouch>())
                 {
                     var handle = touch.Handle;
@@ -123,8 +110,6 @@ namespace ARRunner.Xamarin.Game
 
                 if (TwoFingerTouchEvent != null)
                 {
-                    //var firstViewTouchPoint = currentTouches[0].Point;
-                    //var secondViewTouchPoint = currentTouches[1].Point;
                     var firstViewTouchPoint = touchDictionary[touchList[0]];
                     var secondViewTouchPoint = touchDictionary[touchList[1]];
 
@@ -155,84 +140,6 @@ namespace ARRunner.Xamarin.Game
 
                 var touchData = new TwoFingerTouch(0, firstViewTouchPoint, secondViewTouchPoint, GestureState.Change);
                 TwoFingerTouchEvent(this, new EventArgs<TwoFingerTouch>(touchData));
-
-
-                // https://developer.xamarin.com/guides/ios/application_fundamentals/touch/touch_tracking/
-                //if(touches.Count == 1)
-                //{
-                //    if (TwoFingerTouchEvent != null)
-                //    {
-                //        var newTouches = touches.Cast<UITouch>().Select(a => (a.Handle, a.LocationInView(view))).ToList();
-                //        if(currentTouches[0].Id == newTouches[0].Handle)
-                //        {
-                //            currentTouches[0] = newTouches[0];
-                //        }
-                //        else 
-                //        {
-                //            currentTouches[1] = newTouches[0];
-                //        }
-
-                //        var firstViewTouchPoint = currentTouches[0].Point;
-                //        var secondViewTouchPoint = currentTouches[1].Point;
-                //        var touchData = new TwoFingerTouch(0, firstViewTouchPoint, secondViewTouchPoint, GestureState.Cancelled);
-
-                //        //var firstTouchData = (UITouch)touches.ElementAt(0);
-                //        //var secondTouchData = (UITouch)touches.ElementAt(0);
-                //        //if (firstTouchData == currentTouches[0])
-                //        //{
-                //        //     secondTouchData = currentTouches[1];
-                //        //}
-                //        //else
-                //        //{
-                //        //    firstTouchData = currentTouches[1];
-                //        //}
-
-                //        //var viewTouchPoint1 = /*firstTouchData*/ currentTouches[0].LocationInView(view);
-                //        //var viewTouchPoint2 = /*secondTouchData*/ currentTouches[1].LocationInView(view);
-
-                //        Debug.WriteLine("TouchesMoved(1): firstViewTouchPoint" + firstViewTouchPoint.ToString() + ", secondViewTouchPoint: " + secondViewTouchPoint.ToString());
-
-                //        //var touchData = new TwoFingerTouch(0, viewTouchPoint1, viewTouchPoint2, GestureState.Change);
-                //        TwoFingerTouchEvent(this, new EventArgs<TwoFingerTouch>(touchData));
-                //    }
-                //}
-                //if (touches.Count == 2)
-                //{
-                //    if (TwoFingerTouchEvent != null)
-                //    {
-                //        var newTouches = touches.Cast<UITouch>().Select(a => (a.Handle, a.LocationInView(view))).ToList();
-                //        if (currentTouches[0].Id == newTouches[0].Handle)
-                //        {
-                //            currentTouches[0] = newTouches[0];
-                //            currentTouches[1] = newTouches[1];
-                //        }
-                //        else
-                //        {
-                //            currentTouches[0] = newTouches[1];
-                //            currentTouches[1] = newTouches[0];
-                //        }
-
-                //        var firstViewTouchPoint = currentTouches[0].Point;
-                //        var secondViewTouchPoint = currentTouches[1].Point;
-                //        var touchData = new TwoFingerTouch(0, firstViewTouchPoint, secondViewTouchPoint, GestureState.Cancelled);
-
-                //        //var firstTouchData = (UITouch)touches.ElementAt(0);
-                //        //var secondTouchData = (UITouch)touches.ElementAt(1);
-                //        //if (firstTouchData == currentTouches[1])
-                //        //{
-                //        //    firstTouchData = (UITouch)touches.ElementAt(1);
-                //        //    secondTouchData = (UITouch)touches.ElementAt(0);
-                //        //}
-
-                //        //var viewTouchPoint1 = /*firstTouchData*/ currentTouches[0].LocationInView(view);
-                //        //var viewTouchPoint2 = /*secondTouchData*/ currentTouches[1].LocationInView(view);
-
-                //        Debug.WriteLine("TouchesMoved(2): firstViewTouchPoint" + firstViewTouchPoint.ToString() + ", secondViewTouchPoint: " + secondViewTouchPoint.ToString());
-
-                //        //var touchData = new TwoFingerTouch(0, viewTouchPoint1, viewTouchPoint2, GestureState.Change);
-                //        TwoFingerTouchEvent(this, new EventArgs<TwoFingerTouch>(touchData));
-                //    }
-                //}
             }
         }
 
@@ -243,7 +150,7 @@ namespace ARRunner.Xamarin.Game
 
             if (touches.Count == 1 && currentTouchType == TouchType.SingleTouch)
             {
-                Debug.WriteLine("TouchesEnded (TouchType.SingleTouch): " + touches.Count);
+                //Debug.WriteLine("TouchesEnded (TouchType.SingleTouch): " + touches.Count);
                 if (SingleFingerTouchEvent != null)
                 {
                     var touch = (UITouch)touches.First();
@@ -257,26 +164,7 @@ namespace ARRunner.Xamarin.Game
             {
                 if(TwoFingerTouchEvent != null)
                 {
-                    //var newTouches = touches.Cast<UITouch>().Select(a => (a.Handle, a.LocationInView(view))).ToList();
-                    //if (currentTouches[0].Id == newTouches[0].Handle)
-                    //{
-                    //    currentTouches[0] = newTouches[0];
-                    //    currentTouches[1] = newTouches[1];
-                    //}
-                    //else
-                    //{
-                    //    currentTouches[0] = newTouches[1];
-                    //    currentTouches[1] = newTouches[0];
-                    //}
-
-                    //var firstViewTouchPoint = currentTouches[0].Point;
-                    //var secondViewTouchPoint = currentTouches[1].Point;
-
-
-                    //var firstTouchData = currentTouches[0];
-                    //var secondTouchData = currentTouches[1];
-
-                    Debug.WriteLine("TouchesEnded");
+                    //Debug.WriteLine("TouchesEnded");
 
                     var firstViewTouchPoint = touchDictionary[touchList[0]];
                     var secondViewTouchPoint = touchDictionary[touchList[1]];
---
+++ ARRunner.Xamarin/Game/RunnerPhysics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ARRunner.Xamarin.Game
+{
+    //http://www.softschools.com/formulas/physics/air_resistance_formula/85/
+    public class RunnerPhysics
+    {
+        // https://en.wikipedia.org/wiki/Density_of_air
+        const double DensityAir = 1.225d; // kg/m3
+
+        // http://www.taylors.edu.my/EURECA/2014/downloads/02.pdf
+        const double DragCoeffHumanBodyStart = 0.6f;      // Squating
+        const double DragCoeffHumanBodyRunning = 1.2f;    // Standing
+
+        const double AreaHumanBodyStart = 0.6f;      // Estimate
+        const double AreaHumanBodyRunning = 1f;      // Height * Width = 2 * 0.5
+
+        double _currentSpeed = 0.0f;
+
+        public double BodyMassInKg {
+            get;
+            set;
+        }
+
+        public void ApplyForce(double force){
+            // This is a simplefied model.
+
+            // The dragforce is
+            // F = ((density * dragCoeff * Area) / 2) * speed^2
+            double dragForce = ((DensityAir * DragCoeffHumanBodyRunning * AreaHumanBodyRunning) / 2) * Math.Pow(_currentSpeed, 2);
+
+            double resultingForce = force - dragForce;
+        }
+    }
+}
